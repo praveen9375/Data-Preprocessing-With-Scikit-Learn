d2960071bcc5a54789eb48e714a0d6baebff0acb
praveen9375 <48412124+praveen9375@users.noreply.github.com>
2021-05-22 16:13:47 +0545

    Update DataRange_prp.py

--- DataRange_prp.py
+++ DataRange_prp.py
@@ -1,4 +1,4 @@
-#The coding exercise in this chapter uses MinMaxScaler (imported in backend) to complete the ranged_data function.
+#The exercise here uses MinMaxScaler (imported in backend) to complete the ranged_data function.
 #The function will compress the input NumPy array, data, into the range given by value_range.
 def ranged_data(data, value_range):
   min_max_scaler = MinMaxScaler(feature_range = value_range)
