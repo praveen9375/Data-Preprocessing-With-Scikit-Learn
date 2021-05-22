668b0d4291cce0aa370e8690fd3a5a0d8a495365
praveen9375 <48412124+praveen9375@users.noreply.github.com>
2021-05-22 16:11:18 +0545

    Create Standardingdata_prp.py

---
+++ Standardingdata_prp.py
@@ -0,0 +1,5 @@
+#The coding exercise in this chapter is to complete a generic data standardization function, standardize_data.
+#This function will standardize the input NumPy array, data, by using the scale function (imported in the backend).
+def standardize_data(data):
+  scaled_data = scale (data)
+  return scaled_data
